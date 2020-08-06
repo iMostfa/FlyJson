403e3f90bf29b26440e3ec72a58406e45677c171
Mostfa <almme111@gmail.com>
2020-08-06 18:34:53 +0200

    initial work for the json

---
+++ Sources/App/JsonService/JsonBin.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by mostfa on 8/6/20.
+//
+
+import Foundation
+
+
+
+struct JsonBin: JsonService {
+  func upload(json: String) -> AnyPublisher<String, JsonServiceError> {
+     //will continue later
+  }
+
+  
+}
---
+++ Sources/App/JsonService/JsonService.swift
@@ -0,0 +1,24 @@
+//
+//  JsonService.swift
+//  
+//
+//  Created by mostfa on 8/6/20.
+//
+
+import Foundation
+import Combine
+
+
+protocol JsonService {
+   func upload(json: String) -> AnyPublisher<String,JsonServiceError>
+}
+
+
+enum JsonServiceError: Error {
+  case unDefinded
+  case never
+}
+
+
+
+enum JsonUploaders {}
